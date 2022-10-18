fe4f17d5bcc3ef97598a59962b142ccfe770abf0
Stuart McAlpine <stuart.mcalpine@helsinki.fi>
2022-10-18 16:16:59 +0200

    Adding benchmark models

--- bias_bench/io.py
+++ bias_bench/io.py
@@ -4,13 +4,10 @@ import numpy as np
 
 class BiasModelData:
 
-    def __init__(self, fname, model_name="myModel"):
+    def __init__(self, params):
 
-        # Location of HDF5 file.
-        self.fname = fname
-
-        # Name of this model.
-        self.model_name = model_name
+        # Parameters from parameter file.
+        self.params = params
 
         # Load the data.
         self._load_bias_model_data()
@@ -21,7 +18,7 @@ class BiasModelData:
         self.info = {}
         _required_header_params = ['BoxSize', 'GridSize']
 
-        with h5py.File(self.fname, "r") as f:
+        with h5py.File(self.params['hdf5_file_path'], "r") as f:
             # Read the meta-data from the header
             for att in f['Header'].attrs.keys():
                 self.info[att] = f['Header'].attrs.get(att)
@@ -32,22 +29,19 @@ class BiasModelData:
 
             # The over density field (NxNxN)
             # TODO: consistent naming
-            self.overdensity_field = f['overdensity_field'][...]
+            self.overdensity_field = f[self.params['overdensity_field_name']][...]
             print(f"Loaded overdensity field shape={self.overdensity_field.shape}")
             assert np.all(self.overdensity_field.shape == self.info['GridSize'])
 
             # Count field (predicted)
-            if 'count_field' in f:
-                self.count_field = f['count_field'][...]
+            if self.params['count_field_name'] in f:
+                self.count_field = f[self.params['count_field_name']][...]
                 print(f"Loaded predicted count field shape={self.count_field.shape}")
                 assert self.overdensity_field.shape == self.count_field.shape
 
             # Count field (truth)
-            if 'count_field_truth' in f:
-                self.count_field_truth = f['count_field_truth'][...]
+            if self.params['count_field_truth_name'] in f:
+                self.count_field_truth = f[self.params['count_field_truth_name']][...]
                 print(f"Loaded count field truth shape={self.count_field_truth.shape}")
                 assert self.overdensity_field.shape == self.count_field_truth.shape
 
-
-if __name__ == '__main__':
-    BM = BiasModelData("../mock_data/gadget_mock.hdf5")
