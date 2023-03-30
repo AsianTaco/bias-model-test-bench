957785f7dbe001bcfee37854fda42dbff1264a28
Simon Ding <simon.ding@iap.fr>
2023-03-30 12:11:07 +0200

    Enabled three point statistic computation again

--- src/bias_bench/analysis/three_point.py
+++ src/bias_bench/analysis/three_point.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from bias_bench.data_io import BiasModelData
+from bias_bench.constants import *
 
 
 def compute_bispectrum(field, Lbox, k1, k2, Ntheta, MAS):
@@ -22,8 +23,7 @@ def compute_bispectrum(field, Lbox, k1, k2, Ntheta, MAS):
     return theta, {'bispectrum': bbk.B, 'reduced_bispectrum': bbk.Q}
 
 
-def plot_bispectrum(bias_model_list: Sequence[BiasModelData], params):
-    # TODO: Find out what k1, k2 values would be sensible so set as default
+def plot_bispectrum(bias_model_list: Sequence[BiasModelData], params, dir_path):
     show_density = params['bi_spectrum']['show_density']
     k1 = params['bi_spectrum']['k1']
     k2 = params['bi_spectrum']['k2']
@@ -31,41 +31,74 @@ def plot_bispectrum(bias_model_list: Sequence[BiasModelData], params):
     MAS = params['bi_spectrum']['MAS']
 
     for bias_model_index, bias_model_data in enumerate(bias_model_list):
-        fig, ax = plt.subplots()
-
-        l_box = bias_model_data.info['BoxSize']
         bias_model_name = params[f'bias_model_{bias_model_index + 1}']['name']
         benchmark_model_name = params[f'bias_model_{bias_model_index + 1}']['count_field_benchmark_name']
 
-        try:
-            count_field = bias_model_data.count_field
-            k_counts, bispec_counts = compute_bispectrum(count_field, l_box, k1, k2, Ntheta, MAS=MAS)
-            ax.loglog(k_counts, bispec_counts['bispectrum'], label='predicted')
-        except AttributeError:
-            print("No predicted count field found in BiasModelData. Skipping plots")
+        for res_i in range(bias_model_data.n_res):
+            for mass_bin_i in range(bias_model_data.n_mass_bins):
 
-        try:
-            ground_truth = bias_model_data.count_field_truth
-            k_truth, bispec_truth = compute_bispectrum(ground_truth, l_box, k1, k2, Ntheta, MAS=MAS)
-            ax.loglog(k_truth, bispec_truth['bispectrum'], label='ground truth')
-        except AttributeError:
-            print("No ground truth count field found in BiasModelData. Skipping plots")
+                fig, ax = plt.subplots()
+                fig_ratio, ax_ratio = plt.subplots()
 
-        try:
-            benchmark = bias_model_data.count_field_benchmark
-            k_benchmark, bispec_benchmark = compute_bispectrum(benchmark, l_box, k1, k2, Ntheta, MAS=MAS)
-            ax.loglog(k_benchmark, bispec_benchmark['bispectrum'], label=benchmark_model_name)
-        except AttributeError:
-            print("No benchmark count field found in BiasModelData. Skipping plots")
+                ground_truth_field_exists = False
+                l_box = bias_model_data.info[f'{res_base_name}_{res_i}'][f'{box_size_attr}']
 
-        if show_density:
-            overdensity_field = bias_model_data.overdensity_field
-            k_density, bispect_density = compute_bispectrum(overdensity_field, l_box, k1, k2, Ntheta, MAS=MAS)
-            ax.loglog(k_density, bispect_density['bispectrum'], label="density")
+                for sim_i in range(bias_model_data.n_simulations):
 
-        ax.set_xlabel(r"Angle $\theta$")
-        ax.set_ylabel(r"$P(k)$ [$h^{-3}\mathrm{Mpc}^3$]")
-        ax.legend()
-        fig.suptitle(bias_model_name)
-        fig.tight_layout(rect=[0, 0.03, 1, 0.95])
-        fig.savefig(f"{params['out_dir']}/plots/three_point_{bias_model_name}.png")
+                    if show_density:
+                        overdensity_field = bias_model_data.dm_overdensity_fields[sim_i][res_i][mass_bin_i]
+                        k_density, bispect_density = compute_bispectrum(overdensity_field, l_box, k1, k2, Ntheta,
+                                                                        MAS=MAS)
+                        ax.loglog(k_density, bispect_density['bispectrum'], label="density")
+
+                    try:
+                        count_field_truth = bias_model_data.count_fields_truth[sim_i][res_i][mass_bin_i]
+                        count_overdensity_truth = count_field_truth / np.mean(count_field_truth) - 1
+                        k_truth, bispec_truth = compute_bispectrum(count_overdensity_truth, l_box, k1, k2, Ntheta,
+                                                                   MAS=MAS)
+                        ax.loglog(k_truth, bispec_truth['bispectrum'], label='ground truth')
+                        ground_truth_field_exists = True
+                    except IndexError:
+                        print("No ground truth count field found in BiasModelData. Skipping plots")
+
+                    try:
+                        count_field = bias_model_data.count_fields_predicted[sim_i][res_i][mass_bin_i]
+                        count_overdensity = count_field / np.mean(count_field) - 1
+                        k_counts, bispec_counts = compute_bispectrum(count_overdensity, l_box, k1, k2, Ntheta, MAS=MAS)
+                        ax.loglog(k_counts, bispec_counts['bispectrum'], label='predicted')
+
+                        if ground_truth_field_exists:
+                            ax_ratio.loglog(k_truth, bispec_counts['bispectrum'] / bispec_truth['bispectrum'],
+                                            label=f'predicted ({bias_model_name})')
+                    except IndexError:
+                        print("No predicted count field found in BiasModelData. Skipping plots")
+
+                    try:
+                        count_field_benchmark = bias_model_data.counts_field_benchmark[sim_i][res_i][mass_bin_i]
+                        count_overdensity_benchmark = count_field_benchmark / np.mean(count_field_benchmark) - 1
+                        k_benchmark, bispec_benchmark = compute_bispectrum(count_overdensity_benchmark, l_box, k1, k2,
+                                                                           Ntheta, MAS=MAS)
+                        ax.loglog(k_benchmark, bispec_benchmark['bispectrum'], label=benchmark_model_name)
+
+                        if ground_truth_field_exists:
+                            ax_ratio.loglog(k_truth, bispec_benchmark['bispectrum'] / bispec_truth['bispectrum'],
+                                            label=f'{benchmark_model_name}')
+
+                    except AttributeError:
+                        print("No benchmark count field found in BiasModelData. Skipping plots")
+
+                ax.set_xlabel(r"Angle $\theta$")
+                ax.set_ylabel(r"$B(k)$ [$h^{-3}\mathrm{Mpc}^3$]")
+                ax.legend()
+                fig.suptitle(bias_model_name)
+                fig.tight_layout(rect=[0, 0.03, 1, 0.95])
+                fig.savefig(f"{dir_path}/{bias_model_name}_res_{res_i}_mass_{mass_bin_i}.png")
+
+                if ground_truth_field_exists:
+                    ax_ratio.set_xlabel(r"Angle $\theta$")
+                    ax_ratio.set_ylabel(r"$B(k) / B_{truth}(k)$ ")
+                    ax_ratio.axhline(1, linewidth=.5, linestyle='--', color='black')
+                    ax_ratio.set_ylim(1e-1, 1e1)
+                    ax_ratio.legend()
+                    fig_ratio.tight_layout(pad=.1)
+                    fig_ratio.savefig(f"{dir_path}/ratios_res_{res_i}_mass_{mass_bin_i}.png")
--- src/bias_bench/plot.py
+++ src/bias_bench/plot.py
@@ -27,7 +27,9 @@ def plot_bias_model_metrics(bias_model_data: Sequence[BiasModelData], bias_param
         dir_path = f"{parent_folder_path}/two_point"
         Path(dir_path).mkdir(parents=True, exist_ok=True)
         plot_power_spectrum(bias_model_data, params, dir_path)
-    # if 'bi_spectrum' in params['plots']:
-    #     plot_bispectrum(bias_model_data, params)
+    if 'bi_spectrum' in params['plots']:
+        dir_path = f"{parent_folder_path}/three_point"
+        Path(dir_path).mkdir(parents=True, exist_ok=True)
+        plot_bispectrum(bias_model_data, params, dir_path)
     # if 'density_images' in params['plots']:
     #     plot_density_field(bias_model_data, params)
