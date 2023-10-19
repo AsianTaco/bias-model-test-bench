3bbc271e72e2bf743e79dfc64c232556d647baba
Simon Ding <simon.ding@iap.fr>
2023-10-19 19:38:20 +0200

    Better benchmark model class; refactored truncated power law model; Updated example config

--- src/bias_bench/benchmark_models/BenchmarkModel.py
+++ src/bias_bench/benchmark_models/BenchmarkModel.py
@@ -2,6 +2,28 @@ from abc import ABC, abstractmethod
 
 
 class BenchmarkModel(ABC):
+    """
+    Abstract class for benchmark models.
+
+    ...
+
+    Attributes
+    ----------
+    name : str
+        name of the model
+    n_params : int
+        number of parameters in the model
+    bounds : list
+        lower and upper bounds of each parameter given as a tuple
+    age : int
+        age of the person
+
+    Methods
+    -------
+    info(additional=""):
+        Prints the person's name and age.
+    """
+
     bounds = []
 
     def __init__(self, name, n_params):
@@ -9,6 +31,37 @@ class BenchmarkModel(ABC):
         self.n_params = n_params
 
     pass
+
+    def check_bounds(self, params):
+        """
+        Function to check that the given parameters are within the bounds of the model
+
+        Parameters
+        ----------
+
+        bounds: list
+            A list of tuples specifying the lower and upper bounds of the parameter.
+        params: np.ndarray
+            Array of parameters to be checked if they are within the given bounds
+
+        Returns
+        -------
+        is_out_of_bounds : bool
+            Boolean value if the set of parameters is out of bounds
+        """
+
+        assert self.n_params == params.size
+        assert self.n_params == len(self.bounds)
+
+        is_out_of_bounds = False
+
+        for idx, param in enumerate(params):
+            lower, upper = self.bounds[idx]
+
+            if param < lower or param > upper:
+                is_out_of_bounds= True
+
+        return is_out_of_bounds
 
     @abstractmethod
     def predict(self, input_xs, params):
--- src/bias_bench/benchmark_models/TruncatedPowerLaw.py
+++ src/bias_bench/benchmark_models/TruncatedPowerLaw.py
@@ -8,61 +8,63 @@ class TruncatedPowerLaw(BenchmarkModel):
         super().__init__("Truncated Power Law", 4)
         self.bounds = [(1., 1e8), (1e-6, 6.), (1e-6, 3.), (1e-6, 1e5)]
 
-    def get_mean_ngal(self, rho, nmean, beta, epsilon_g, rho_g):
+    def get_mean_ngal(self, delta_m, nmean, beta, epsilon_g, rho_g):
         """
-        Using the truncated power law bias model, compute the mean number of
-        galaxies expected for a given density.
+        Using the truncated power law bias model (Neyrinck et al. 2014) to compute
+        the mean number of galaxies expected for given over-density :math:`\\delta_m`:
+
+        .. math::
+            n_{gal} = n_{mean} \\rho_m^\\beta \\exp[(\\rho_m / \\rho_g)^{-\\epsilon}],
+
+        where :math:`\\rho_m = \\delta_m + 1`.
 
         Incoming density field is expected to be 1D, i.e., the flattened 3D array.
 
         Parameters
         ----------
-        rho : ndarray
+        delta_m : ndarray
             The 3D density field
         nmean : float
-            ...
+            First bias parameter controlling the overall amplitude of the function.
         beta : float
             ...
-        epsilon_g
+        epsilon_g : float
             ...
         rho_g : float
             ...
 
         Returns
         -------
-        ngal_mean : ndarray
-            Expected number of galaxies for the given densities
+        ngal_mean : np.ndarray
+            Expected number of galaxies for the given densities.
         """
 
-        # TODO: refactor this
-
-        nmean_bounds = self.bounds[0]
-        if nmean < nmean_bounds[0] or nmean > nmean_bounds[1]:
+        if self.check_bounds(np.asarray([nmean, beta, epsilon_g, rho_g])):
             return np.NaN
 
-        beta_bounds = self.bounds[1]
-        if beta < beta_bounds[0] or beta > beta_bounds[1]:
-            return np.NaN
+        # # result array
+        # ngal_mean = np.zeros_like(delta_m)
+        #
+        # # Non zero densites
+        # idx = np.where(delta_m > -1.0)
+        #
+        # d = 1 + delta_m[idx]
+        # x = (d / rho_g) ** (-epsilon_g)
+        # ngal_mean[idx] = nmean * d ** beta * np.exp(-x)
 
-        epsilon_g_bounds = self.bounds[2]
-        if epsilon_g < epsilon_g_bounds[0] or epsilon_g > epsilon_g_bounds[1]:
-            return np.NaN
-
-        rho_g_bounds = self.bounds[3]
-        if rho_g < rho_g_bounds[0] or rho_g > rho_g_bounds[1]:
-            return np.NaN
-
-        # Return array
-        ngal_mean = np.zeros_like(rho)
-
-        # Non zero densites
-        idx = np.where(rho > -1.0)
-
-        d = 1 + rho[idx]
-        x = (d / rho_g) ** (-epsilon_g)
-        ngal_mean[idx] = nmean * d ** beta * np.exp(-x)
+        rho_m = 1 + delta_m + 1e-6
+        x = (rho_m / rho_g) ** (-epsilon_g)
+        ngal_mean = nmean * (rho_m ** beta) * np.exp(-x) + 1e-6
 
         return ngal_mean
+
+    def adj_gradient_mean_ngal(self, delta_m, gradient, nmean, alpha, epsilon, rho_g):
+        rho_m = 1 + delta_m + 1e-6
+        a = rho_m ** (alpha - 1)
+        b = (rho_m / rho_g) ** (-epsilon)
+        f = np.exp(-b)
+
+        return (alpha + epsilon * b) * a * f * nmean * gradient
 
     def predict(self, delta, popt):
         # TODO: implement bounds explicitly
