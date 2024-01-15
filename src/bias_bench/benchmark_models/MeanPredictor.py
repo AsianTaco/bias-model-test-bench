b694643b5868f24685501f92a1693461ec68156e
Simon Ding <simon.ding@iap.fr>
2024-01-15 17:20:49 +0100

    new 'bias' model for enabling likelihood fits

---
+++ src/bias_bench/benchmark_models/MeanPredictor.py
@@ -0,0 +1,12 @@
+from bias_bench.benchmark_models.BenchmarkModel import BenchmarkModel
+
+
+class MeanPredictor(BenchmarkModel):
+
+    def __init__(self):
+        super().__init__("Mean predictor", 1)
+        self.bounds = [(0, 1e8)]
+
+    # TODO: Extend this to be aware of the underlying overdensity
+    def predict(self, delta, popt):
+        return popt
